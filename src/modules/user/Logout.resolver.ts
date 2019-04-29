8901498d85691634dbc90547616dcec6bb9277ea
Chris Eady <chris.eady1@gmail.com>
2019-04-29 09:28:05 -0500

    Clear cookie on logout

--- src/modules/user/Logout.resolver.ts
+++ src/modules/user/Logout.resolver.ts
@@ -1,19 +1,22 @@
-import { Resolver, Ctx, Mutation } from 'type-graphql';
-import { MyContext } from '../../types/MyContext';
+import { Resolver, Ctx, Mutation } from 'type-graphql'
+import { MyContext } from '../../types/MyContext'
+import { SESS_NAME } from '../../config'
 
 @Resolver()
 export class LogoutResolver {
   @Mutation(() => Boolean)
-  async logout(@Ctx() ctx: MyContext) {
+  async logout(@Ctx() { req, res }: MyContext): Promise<boolean> {
     return new Promise((resolve, reject) =>
-      ctx.req.session!.destroy(err => {
+      req.session!.destroy(err => {
         if (err) {
-          console.log(err);
-          reject(false);
+          console.log(err)
+          return reject(false)
         }
 
-        resolve(true);
+        res.clearCookie(SESS_NAME)
+
+        return resolve(true)
       })
-    );
+    )
   }
 }
