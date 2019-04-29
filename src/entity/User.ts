f51cc8174e5283e8c7fb5edd49ac3b32935f9fcd
Chris Eady <chris.eady1@gmail.com>
2019-04-29 09:42:10 -0500

    Add email to user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -5,52 +5,56 @@ import {
   PrimaryGeneratedColumn,
   BeforeInsert,
   BeforeUpdate,
-} from 'typeorm';
-import { ObjectType, Field, ID, Root } from 'type-graphql';
-import { hash } from 'bcryptjs';
+} from 'typeorm'
+import { ObjectType, Field, ID, Root } from 'type-graphql'
+import { hash } from 'bcryptjs'
 
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
   @Field(() => ID)
   @PrimaryGeneratedColumn('uuid')
-  id: string;
+  id: string
 
   @Field()
   @Column({ unique: true })
-  netId: string;
+  netId: string
 
   @Field()
   @Column({ unique: true })
-  nineDigitId: string;
+  nineDigitId: string
 
   @Field()
   @Column()
-  firstName: string;
+  firstName: string
 
   @Field()
   @Column()
-  lastName: string;
+  lastName: string
 
   @Field({ complexity: 3 })
   name(@Root() parent: User): string {
-    return `${parent.firstName} ${parent.lastName}`;
+    return `${parent.firstName} ${parent.lastName}`
   }
 
   @Field()
   @Column({ default: false })
-  admin: boolean;
+  admin: boolean
 
   @Field()
   @Column({ default: true })
-  active: boolean;
+  active: boolean
+
+  @Field()
+  @Column('text', { unique: true })
+  email: string
 
   @Column()
-  password: string;
+  password: string
 
   @BeforeInsert()
   @BeforeUpdate()
   async hashPassword() {
-    this.password = await hash(this.password, 12);
+    this.password = await hash(this.password, 12)
   }
 }
