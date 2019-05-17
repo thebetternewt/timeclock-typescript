d499b8c191895e387578ac41881b7246b9a0049c
Chris Eady <chris.eady1@gmail.com>
2019-05-17 16:10:17 -0500

    Add year filter to WorkStudyPeriod resolver

--- src/modules/workStudyPeriod/WorkStudyPeriod.resolver.ts
+++ src/modules/workStudyPeriod/WorkStudyPeriod.resolver.ts
@@ -1,11 +1,23 @@
-import { Resolver, Query, Arg, ID } from 'type-graphql';
+import { Resolver, Query, Arg, ID, Int } from 'type-graphql';
 import { WorkStudyPeriod } from '../../entity/WorkStudyPeriod';
 
 @Resolver()
 export class AllWorkStudyPeriodResolver {
   @Query(() => [WorkStudyPeriod])
-  async allWorkStudyPeriod(): Promise<WorkStudyPeriod[]> {
-    const periods = await WorkStudyPeriod.find({});
+  async allWorkStudyPeriod(
+    @Arg('year', () => Int, { nullable: true }) year?: number
+  ): Promise<WorkStudyPeriod[]> {
+    interface WorkStudyPeriodSearchParams {
+      year?: number;
+    }
+
+    const searchParams: WorkStudyPeriodSearchParams = {};
+
+    if (year) {
+      searchParams.year = year;
+    }
+
+    const periods = await WorkStudyPeriod.find(searchParams);
 
     return periods;
   }
