b93301832909eb887aba0cf7dd4e7f0e8b3d7314
Chris Eady <chris.eady1@gmail.com>
2019-07-02 16:30:07 -0500

    Allow supervisors to delete shifts

--- src/modules/shift/Shifts.resolver.ts
+++ src/modules/shift/Shifts.resolver.ts
@@ -1,18 +1,18 @@
 import { MyContext } from './../../types/MyContext';
 import {
-	Between,
-	FindOperator,
-	MoreThanOrEqual,
-	LessThanOrEqual,
+  Between,
+  FindOperator,
+  MoreThanOrEqual,
+  LessThanOrEqual,
 } from 'typeorm';
 import {
-	Resolver,
-	Query,
-	UseMiddleware,
-	Arg,
-	Mutation,
-	ID,
-	Ctx,
+  Resolver,
+  Query,
+  UseMiddleware,
+  Arg,
+  Mutation,
+  ID,
+  Ctx,
 } from 'type-graphql';
 
 import { isAdmin } from '../middleware/isAdmin';
@@ -26,111 +26,120 @@ import { User } from '../../entity/User';
 
 @Resolver()
 export class ShiftsResolver {
-	@UseMiddleware(isAuth)
-	@Query(() => [Shift])
-	async shifts(
-		@Ctx() ctx: MyContext,
-		@Arg('userId', () => ID, { nullable: true }) userId?: string,
-		@Arg('deptId', () => ID, { nullable: true }) deptId?: string,
-		@Arg('startDate', { nullable: true }) startDate?: Date,
-		@Arg('endDate', { nullable: true }) endDate?: Date
-	): Promise<Shift[]> {
-		const { isAdmin, userId: currentUserId } = ctx.req.session!;
+  @UseMiddleware(isAuth)
+  @Query(() => [Shift])
+  async shifts(
+    @Ctx() ctx: MyContext,
+    @Arg('userId', () => ID, { nullable: true }) userId?: string,
+    @Arg('deptId', () => ID, { nullable: true }) deptId?: string,
+    @Arg('startDate', { nullable: true }) startDate?: Date,
+    @Arg('endDate', { nullable: true }) endDate?: Date
+  ): Promise<Shift[]> {
+    const { isAdmin, userId: currentUserId } = ctx.req.session!;
 
-		if (!isAdmin && userId !== currentUserId && !isSupervisor(ctx, deptId)) {
-			throw new ForbiddenError('Not authorized...');
-		}
+    if (!isAdmin && userId !== currentUserId && !isSupervisor(ctx, deptId)) {
+      throw new ForbiddenError('Not authorized...');
+    }
 
-		interface ShiftSearchParams {
-			userId?: string;
-			deptId?: string;
-			timeIn?: FindOperator<any> | Date;
-		}
+    interface ShiftSearchParams {
+      userId?: string;
+      deptId?: string;
+      timeIn?: FindOperator<any> | Date;
+    }
 
-		const searchParams: ShiftSearchParams = {};
+    const searchParams: ShiftSearchParams = {};
 
-		if (userId) {
-			searchParams.userId = userId;
-		}
+    if (userId) {
+      searchParams.userId = userId;
+    }
 
-		if (deptId) {
-			searchParams.deptId = deptId;
-		}
+    if (deptId) {
+      searchParams.deptId = deptId;
+    }
 
-		if (startDate && endDate) {
-			console.log('start:', startDate, 'end:', endDate);
-			searchParams.timeIn = Between(startDate, endDate);
-		} else if (startDate) {
-			searchParams.timeIn = MoreThanOrEqual(startDate);
-		} else if (endDate) {
-			searchParams.timeIn = LessThanOrEqual(endDate);
-		}
+    if (startDate && endDate) {
+      console.log('start:', startDate, 'end:', endDate);
+      searchParams.timeIn = Between(startDate, endDate);
+    } else if (startDate) {
+      searchParams.timeIn = MoreThanOrEqual(startDate);
+    } else if (endDate) {
+      searchParams.timeIn = LessThanOrEqual(endDate);
+    }
 
-		return Shift.find({ where: searchParams, order: { timeIn: 'ASC' } });
-	}
+    return Shift.find({ where: searchParams, order: { timeIn: 'ASC' } });
+  }
 }
 
 @Resolver()
 export class CreateShiftResolver {
-	@UseMiddleware(isAuth)
-	@Mutation(() => Shift)
-	async createShift(
-		@Ctx() ctx: MyContext,
-		@Arg('data') data: ShiftInput
-	): Promise<Shift> {
-		// Throw error if current user not admin or supervisor of shift department.
-		if (!isSupervisor(ctx, data.deptId)) {
-			throw new ForbiddenError('Not authorized!');
-		}
+  @UseMiddleware(isAuth)
+  @Mutation(() => Shift)
+  async createShift(
+    @Ctx() ctx: MyContext,
+    @Arg('data') data: ShiftInput
+  ): Promise<Shift> {
+    // Throw error if current user not admin or supervisor of shift department.
+    if (!isSupervisor(ctx, data.deptId)) {
+      throw new ForbiddenError('Not authorized!');
+    }
 
-		return Shift.create(data).save();
-	}
+    return Shift.create(data).save();
+  }
 }
 
 @Resolver()
 export class UpdateShiftResolver {
-	@UseMiddleware(isAuth)
-	@Mutation(() => Shift)
-	async updateShift(
-		@Ctx() ctx: MyContext,
-		@Arg('id', () => ID) id: string,
-		@Arg('data')
-		{ timeIn, timeOut, userId, deptId }: ShiftInput
-	): Promise<Shift> {
-		const user = await User.findOne(userId);
+  @UseMiddleware(isAuth)
+  @Mutation(() => Shift)
+  async updateShift(
+    @Ctx() ctx: MyContext,
+    @Arg('id', () => ID) id: string,
+    @Arg('data')
+    { timeIn, timeOut, userId, deptId }: ShiftInput
+  ): Promise<Shift> {
+    const user = await User.findOne(userId);
 
-		if (!isCurrentUser(ctx, user!) && !isSupervisor(ctx, deptId)) {
-			throw new ForbiddenError('Not authorized!');
-		}
+    if (!isCurrentUser(ctx, user!) && !isSupervisor(ctx, deptId)) {
+      throw new ForbiddenError('Not authorized!');
+    }
 
-		const shift = await Shift.findOne(id);
+    const shift = await Shift.findOne(id);
 
-		if (!shift) {
-			throw new UserInputError('Shift not found.');
-		}
+    if (!shift) {
+      throw new UserInputError('Shift not found.');
+    }
 
-		shift.timeIn = timeIn;
-		shift.timeOut = timeOut;
-		shift.userId = userId;
-		shift.deptId = deptId;
+    shift.timeIn = timeIn;
+    shift.timeOut = timeOut;
+    shift.userId = userId;
+    shift.deptId = deptId;
 
-		return shift.save();
-	}
+    return shift.save();
+  }
 }
 
 @Resolver()
 export class DeleteShiftResolver {
-	@UseMiddleware(isAdmin)
-	@Mutation(() => Boolean)
-	async deleteShift(@Arg('id', () => ID) id: string): Promise<boolean> {
-		const shift = await Shift.findOne(id);
+  @UseMiddleware(isAuth)
+  @Mutation(() => Boolean)
+  async deleteShift(
+    @Ctx() ctx: MyContext,
+    @Arg('id', () => ID) id: string
+  ): Promise<boolean> {
+    const shift = await Shift.findOne(id);
 
-		if (!shift) {
-			return false;
-		}
+    if (!shift) {
+      return false;
+    }
 
-		await shift.remove();
+    // Only allow delete if current user is admin or supervisor of
+    // department associated with shift.
+    if (!isSupervisor(ctx, shift.deptId)) {
+      throw new ForbiddenError('Not authorized!');
+    }
 
-		return true;
-	}
+    await shift.remove();
+
+    return true;
+  }
 }
